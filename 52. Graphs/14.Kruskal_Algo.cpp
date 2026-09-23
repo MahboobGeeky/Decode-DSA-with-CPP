866d3c42031866872444bd5946df7b283a6ece3a
MahboobGeeky <mahboob9184@gmail.com>
2026-09-23 12:05:19 +0530

    done

--- 52. Graphs/14.Kruskal_Algo.cpp
+++ 52. Graphs/14.Kruskal_Algo.cpp
@@ -52,6 +52,40 @@ public:
 
 };
 
+
+int spanningTree(int V, vector<vector<int>> adj[]){
+
+    vector<pair<int, pair<int, int>>> edges;
+
+    // Convert adjacency list into edge list
+    for(int i=0; i<V; i++){
+        for(auto it : adj[i]){
+            int adjNode = it[0];
+            int wt = it[1];
+            int node = i;
+            edges.push_back({wt, {node, adjNode}});
+
+        }
+    }
+    // Sort edges according to weight
+    sort(edges.begin(), edges.end());
+    DisjointSet ds(V);
+    int mstWt = 0;
+    // Pick edges greedily
+    for(auto it : edges){
+        int wt = it.first;
+        int u = it.second.first;
+        int v = it.second.second;
+        if(ds.findUPar(u) != ds.findUPar(v)){
+            mstWt += wt;
+            ds.unionBySize(u, v);
+
+        }
+    }
+    return mstWt;
+
+}
+
 int main(){
     DisjointSet ds(7);
     ds.unionBySize(1,2);
@@ -68,7 +102,37 @@ int main(){
 
     ds.unionBySize(3,7);
 
-    return 0;
+
+    // KRUSKAL ALGORITHM
+
+
+        // KRUSKAL ALGORITHM
+
+    int V = 5;
+    vector<vector<int>> adj[V];
+
+    adj[0].push_back({1,2});
+    adj[1].push_back({0,2});
+
+    adj[0].push_back({2,1});
+    adj[2].push_back({0,1});
+
+    adj[1].push_back({2,3});
+    adj[2].push_back({1,3});
+
+    adj[1].push_back({3,4});
+    adj[3].push_back({1,4});
+
+    adj[2].push_back({3,5});
+    adj[3].push_back({2,5});
+
+    adj[3].push_back({4,7});
+    adj[4].push_back({3,7});
+
+    cout<<"MST Weight: "<<spanningTree(V, adj)<<endl;
+
 }
+
+
 
 // TIME COMPLEXITY = O(4alpha) equvalent of constant
